f64dd0c26bdeb10f8cd02c9509f8e39263a7d28b
Adam Groner <adamhgroner@gmail.com>
2018-01-07 17:27:21 -0800

    loading and ads first try

---
+++ src/ad.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+class Ad extends React.Component<any> {
+  componentDidMount() {
+    (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
+  }
+
+  render() {
+    return (
+      <div className={`ad ${this.props.className}`}>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block'}}
+          data-ad-client="ca-pub-8427771144228576"
+          data-ad-slot="6830843337"
+          data-ad-format="auto"
+        />
+      </div>
+    );
+  }
+}
+export default styled(Ad)`
+    max-width: 600px;
+    height: 60px;
+    margin: auto;
+`;
